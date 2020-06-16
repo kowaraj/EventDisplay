0dfcd236518a4e6d76b1678bc93c8d08662fd34d
kowaraj <kowaraj4stuff@gmail.com>
2020-06-16 16:42:11 +0200

    Add a local clean-up; Replace the destination with wwwams (was amstest)

--- python-scripts/image-transporter.sh
+++ python-scripts/image-transporter.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
+path_src="/home/common/ams-event-display-snapshots"
+path_dst="/eos/project/w/wwwams/www/buffer"
+timeto_mv=1 #min
+timeto_rm=3 #min
 
 while [ True ] 
 do
-  for x in `find /home/common/ams-event-display-snapshots/ -type f -mmin 1 -printf '%P___%Cs\n'`
+  # Clean up local buffer for older than X min files
+
+  for x in `find ${path_src}/ -type f -mmin +${timeto_rm} -printf '%P___%Cs\n'`
+  do 
+      a=(`echo ${x} | awk -F"___" '{print $1" "$2}'`)
+      echo "CLEAN UP: File name: ${a[0]}, ts= ${a[1]}"
+      rm -rf ${path_src}/${a[0]}
+  done
+
+  # Move to remote buffer
+
+  for x in `find ${path_src}/ -type f -mmin ${timeto_mv} -printf '%P___%Cs\n'`
   do 
       a=(`echo ${x} | awk -F"___" '{print $1" "$2}'`)
       even_ts_half=$(( ${a[1]} /2 ))
       even_ts=$(( even_ts_half*2 ))
-      echo "MOVE: File name: ${a[0]}, ts= ${a[1]} ---> ${even_ts} "
-      mv /home/common/ams-event-display-snapshots/${a[0]} /eos/project-a/amstest/www/buffer/ss_${even_ts}.png
+      echo "MOVE: File name: ${a[0]}, ts= ${a[1]} ---> ${path_dst}/ss_${even_ts}.png "
+      #mv /home/common/ams-event-display-snapshots/${a[0]} /eos/project-a/amstest/www/buffer/ss_${even_ts}.png
+      mv ${path_src}/${a[0]} ${path_dst}/ss_${even_ts}.png
   done
 
-  for x in `find /eos/project-a/amstest/www/buffer/ -type f -mmin +3 -printf '%P___%Cs\n'`
+  # Clean up remote buffer
+
+  for x in `find ${path_dst}/ -type f -mmin +${timeto_rm} -printf '%P___%Cs\n'`
   do 
       a=(`echo ${x} | awk -F"___" '{print $1" "$2}'`)
-      echo "DELETE: File name: ${a[0]} - modified @ ${a[1]}"
-      rm -rf /eos/project-a/amstest/www/buffer/${a[0]}
+      echo "DELETE: File name: ${path_dst}/${a[0]} - modified @ ${a[1]}"
+      rm -rf ${path_dst}/${a[0]}
   done
+
 
   sleep 30
   
